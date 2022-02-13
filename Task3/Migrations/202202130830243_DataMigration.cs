6e8c4acf0803be48ca13f93d8c2e1936feb3101b
DmitriyLeonov <dleonov701@gmail.com>
2022-02-13 13:29:23 +0300

    Initial commit

---
+++ Task3/Migrations/202202130830243_DataMigration.cs
@@ -0,0 +1,20 @@
+﻿namespace Task3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "RegistrationDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.AspNetUsers", "LastLogIn", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "LastLogIn");
+            DropColumn("dbo.AspNetUsers", "RegistrationDate");
+        }
+    }
+}
---
+++ Task3/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Task3.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Task3.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Task3.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Task3.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
